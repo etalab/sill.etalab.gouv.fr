a34a95449db9d65a185c9e1545f1a15a6499c4b8
Bastien <bzg@gnu.org>
2020-03-03 06:59:53 +0100

    Don't use static *html resources as templates
    Fix https://github.com/etalab/sill.etalab.gouv.fr/issues/20

--- src/clj/sillweb/views.clj
+++ src/clj/sillweb/views.clj
@@ -54,65 +54,101 @@
    (response/response (rss-feed))
    :headers {"Content-Type" "text/xml; charset=utf-8"}))
 
-(defn default [lang]
-  (assoc
-   (response/response
-    (io/input-stream
-     (io/resource
-      (str "public/index." lang ".html"))))
-   :headers {"Content-Type" "text/html; charset=utf-8"}))
-
-(defn template [lang title subtitle content]
-  (h/html5
-   {:lang lang}
-   [:head
-    [:title title]
-    [:meta {:charset "utf-8"}]
-    [:meta {:name "keywords" :content (i/i lang [:keywords])}]
-    [:meta {:name "description" :content (i/i lang [:keywords])}]
-    [:meta {:name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=yes"}]
-    (h/include-css "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css")
-    (h/include-css "/css/style.css")]
-   [:body
-    [:nav.navbar {:role "navigation" :aria-label "main navigation"}
-     [:div.navbar-brand
-      [:a.navbar-item {:href "/"}
-       [:img {:src    "/images/logo-marianne.svg"
-              :alt    "Logo Marianne"
-              :width  "120"
-              :height "100"}
-        "sill.etalab.gouv.fr (alpha)"]]]
-     [:div.navbar-end
-      [:div.navbar-menu
-       [:a.navbar-item {:href (str "/" lang "/contributors") :title (i/i lang [:contributors-baseline])} (i/i lang [:contributors])]
-       [:a.navbar-item {:href (str "/" lang "/contact") :title (i/i lang [:contact-baseline])} (i/i lang [:contact])]
-       [:a.navbar-item {:href (str "/" lang "/about") :title (i/i lang [:why-this-website?])} (i/i lang [:about])]
-       [:a.navbar-item {:href  "https://www.etalab.gouv.fr"
-                        :title (i/i lang [:main-etalab-website])} "Etalab"]
-       [:a {:href  "/latest.xml" :target "new"
-            :title (i/i lang [:subscribe-rss-flux])
-            :class "navbar-item button"} [:span {:class "icon"}
-                                         [:i {:class "fas fa-rss"}]]]]]]
-    [:section.hero
-     [:div.hero-body
-      [:div.container
-       [:h1.title.has-text-centered title]
-       [:h2.subtitle.column.is-8.is-offset-2.has-text-centered subtitle]]]]
-    [:section.section
-     [:div.column.is-8.is-offset-2 content]]
-    [:footer.footer
-     [:div.content
-      [:div.columns
-       [:div.column.is-offset-2.is-4
-        [:img {:src "/images/etalab.svg" :width "240px"}]]
-       [:div.column.is-offset-1.is-4
-        [:h1 "sill.etalab.gouv.fr"]
-        [:p (i/i lang [:website-developed-by]) [:a {:href "https://www.etalab.gouv.fr"} "Etalab"]
-         (i/i lang [:source-code-available]) [:a {:href "https://github.com/etalab/sillweb"}
-                                              (i/i lang [:here])]]]]]]]))
+(defn default [lang & [title subtitle content]]
+  (let [title    (or title (i/i lang [:index-title]))
+        subtitle (or subtitle (i/i lang [:index-subtitle]))
+        content0  (if content
+                    [:div {:class "column is-8 is-offset-2"} content]
+                    [:div {:class "column is-10 is-offset-1"}
+                     [:div {:class "container" :id "app"}]])]
+    (h/html5
+     {:lang lang}
+     [:head
+      [:title title]
+      [:meta {:charset "utf-8"}]
+      [:meta {:name "keywords" :content (i/i lang [:keywords])}]
+      [:meta {:name "description" :content (i/i lang [:keywords])}]
+      [:meta {:name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=yes"}]
+      [:link {:rel "canonical" :href "https://sill.etalab.gouv.fr/"}]
+      [:link {:rel "alternate" :type "application/rss+xml"
+              :title (i/i lang [:subscribe-rss-flux])
+              :href "https://sill.etalab.gouv.fr/updates.xml"}]
+      [:meta {:property "og:locale", :content "fr_FR"}]
+      [:meta {:property "og:type", :content "website"}]
+      [:meta {:property "og:title", :content (i/i lang [:index-title])}]
+      [:meta {:property "og:url", :content "https://sill.etalab.gouv.fr/"}]
+      [:meta {:property "og:site_name", :content (i/i lang [:index-title])}]
+      [:meta {:property "og:image", :content "https://www.etalab.gouv.fr/wp-content/uploads/2019/06/etalab-white.png"}]
+      [:meta {:name "twitter:card", :content "summary_large_image"}]
+      [:meta {:name "twitter:title", :content (i/i lang [:index-title])}]
+      [:meta {:name "twitter:site", :content "@Etalab"}]
+      [:meta {:name "twitter:creator", :content "@Etalab"}]
+      (h/include-css "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css")
+      (h/include-css "/css/style.css")]
+      [:script {:type "text/javascript"} "var _paq = window._paq || [];_paq.push(['trackPageView']);_paq.push(['enableLinkTracking']);(function(){var u=\"//stats.data.gouv.fr/\";_paq.push(['setTrackerUrl', u+'piwik.php']);_paq.push(['setSiteId', '112']);var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);})();"]
+      [:noscript [:p [:img {:src "//stats.data.gouv.fr/piwik.php?idsite=112&amp;rec=1"
+                            :alt ""
+                            :style "border:0;"}]]]
+     [:body
+      [:svg {:aria-hidden "true", :focusable "false", :style "display:none"} [:defs  [:symbol {:viewbox "0 0 32 32", :fill-rule "evenodd", :clip-rule "evenodd", :stroke-linejoin "round", :stroke-miterlimit "1.414", :id "copy", :xmlns "http://www.w3.org/2000/svg"} [:path {:d "M19.75 8.5V1H6.625L1 6.625V23.5h11.25V31H31V8.5H19.75zM6.625 3.651v2.974H3.651l2.974-2.974zm-3.75 17.974V8.5H8.5V2.875h9.375V8.5l-5.625 5.625v7.5H2.875zm15-10.474v2.974h-2.974l2.974-2.974zm11.25 17.974h-15V16h5.625v-5.625h9.375v18.75z", :fill-rule "nonzero"}]] [:symbol {:viewbox "0 0 32 32", :fill-rule "evenodd", :clip-rule "evenodd", :stroke-linejoin "round", :stroke-miterlimit "1.414", :id "envelope", :xmlns "http://www.w3.org/2000/svg"} [:g {:fill-rule "nonzero"} [:path {:d "M30.1 5.419c-9.355-.002-18.733-.005-28.1-.005A1.06 1.06 0 0 0 .975 6.439v19.122A1.06 1.06 0 0 0 2 26.586h28a1.061 1.061 0 0 0 1.025-1.025V6.439a1.056 1.056 0 0 0-.925-1.02zM3.025 7.464h25.95v17.072H3.025V7.464z"}] [:path {:d "M30.06 9.513c.933.098 1.382 1.395.393 1.945L16.54 18.287c-.438.188-.479.178-.893 0L1.733 11.458c-1.743-.968-.065-2.254.894-1.842l13.466 6.61 13.562-6.651c.3-.094.312-.062.405-.062z"}]]] [:symbol {:viewbox "0 0 32 32", :fill-rule "evenodd", :clip-rule "evenodd", :stroke-linejoin "round", :stroke-miterlimit "1.414", :id "facebook", :xmlns "http://www.w3.org/2000/svg"} [:path {:d "M28.188 1H3.812A2.821 2.821 0 0 0 1 3.813v24.375A2.821 2.821 0 0 0 3.813 31H16V17.875h-3.75v-3.75H16V12.25a5.635 5.635 0 0 1 5.625-5.625h3.75v3.75h-3.75a1.88 1.88 0 0 0-1.875 1.875v1.875h5.625l-.937 3.75H19.75V31h8.438A2.821 2.821 0 0 0 31 28.187V3.812A2.821 2.821 0 0 0 28.188 1z"}]] [:symbol {:viewbox "0 0 32 32", :fill-rule "evenodd", :clip-rule "evenodd", :stroke-linejoin "round", :stroke-miterlimit "1.414", :id "github", :xmlns "http://www.w3.org/2000/svg"} [:path {:d "M16 1.371c-8.284 0-15 6.715-15 15 0 6.627 4.298 12.25 10.258 14.233.75.138 1.026-.326 1.026-.722 0-.357-.014-1.54-.021-2.793-4.174.907-5.054-1.77-5.054-1.77-.682-1.733-1.665-2.195-1.665-2.195-1.361-.931.103-.912.103-.912 1.506.106 2.299 1.546 2.299 1.546 1.338 2.293 3.509 1.63 4.365 1.247.134-.969.523-1.631.952-2.006-3.331-.379-6.834-1.666-6.834-7.413 0-1.638.586-2.976 1.546-4.027-.156-.378-.669-1.903.145-3.969 0 0 1.26-.403 4.126 1.537a14.453 14.453 0 0 1 3.755-.505c1.274.006 2.558.173 3.757.505 2.864-1.94 4.121-1.537 4.121-1.537.816 2.066.303 3.591.147 3.969.962 1.05 1.544 2.389 1.544 4.027 0 5.761-3.509 7.029-6.849 7.401.538.466 1.017 1.379 1.017 2.778 0 2.007-.018 3.623-.018 4.117 0 .399.27.867 1.03.72C26.707 28.616 31 22.996 31 16.371c0-8.285-6.716-15-15-15z", :fill-rule "nonzero"}]] [:symbol {:viewbox "0 0 32 32", :fill-rule "evenodd", :clip-rule "evenodd", :stroke-linejoin "round", :stroke-miterlimit "1.414", :id "googleplus", :xmlns "http://www.w3.org/2000/svg"} [:path {:d "M28.188 1H3.812A2.821 2.821 0 0 0 1 3.813v24.375A2.821 2.821 0 0 0 3.813 31h24.375A2.821 2.821 0 0 0 31 28.187V3.812A2.821 2.821 0 0 0 28.187 1zM12.251 23.5a7.493 7.493 0 0 1-7.5-7.5c0-4.148 3.352-7.5 7.5-7.5 2.027 0 3.721.738 5.028 1.963l-2.039 1.958c-.557-.534-1.529-1.154-2.989-1.154-2.56 0-4.653 2.121-4.653 4.734s2.092 4.734 4.653 4.734c2.971 0 4.084-2.133 4.254-3.234h-4.253v-2.573h7.084c.064.375.117.75.117 1.243 0 4.289-2.872 7.33-7.201 7.33l-.001-.001zm15-7.5h-1.875v1.875h-1.875V16h-1.875v-1.875h1.875V12.25h1.875v1.875h1.875V16z", :fill-rule "nonzero"}]] [:symbol {:viewbox "0 0 32 32", :fill-rule "evenodd", :clip-rule "evenodd", :stroke-linejoin "round", :stroke-miterlimit "1.414", :id "magnifier", :xmlns "http://www.w3.org/2000/svg"} [:path {:d "M30.07 26.529l-7.106-6.043c-.735-.662-1.521-.964-2.155-.936a11.194 11.194 0 0 0 2.691-7.299c0-6.214-5.036-11.25-11.25-11.25S1 6.037 1 12.251s5.036 11.25 11.25 11.25c2.786 0 5.334-1.012 7.299-2.691-.03.634.274 1.42.936 2.155l6.043 7.106c1.035 1.149 2.725 1.247 3.756.216 1.031-1.032.934-2.723-.216-3.756l.002-.002zm-17.82-6.78a7.5 7.5 0 1 1 0-15 7.5 7.5 0 0 1 0 15z", :fill-rule "nonzero"}]] [:symbol {:viewbox "0 0 32 32", :fill-rule "evenodd", :clip-rule "evenodd", :stroke-linejoin "round", :stroke-miterlimit "1.414", :id "round-cross", :xmlns "http://www.w3.org/2000/svg"} [:path {:d "M16 .5C24.554.5 31.5 7.446 31.5 16S24.554 31.5 16 31.5.5 24.554.5 16 7.446.5 16 .5zm6.161 11.718a7.233 7.233 0 0 0-2.379-2.379L16 13.621l-3.782-3.782a7.233 7.233 0 0 0-2.379 2.379L13.621 16l-3.782 3.782a7.233 7.233 0 0 0 2.379 2.379L16 18.379l3.782 3.782a7.233 7.233 0 0 0 2.379-2.379L18.379 16l3.782-3.782z", :fill-rule "nonzero"}]] [:symbol {:viewbox "0 0 32 32", :fill-rule "evenodd", :clip-rule "evenodd", :stroke-linejoin "round", :stroke-miterlimit "1.414", :id "rss", :xmlns "http://www.w3.org/2000/svg"} [:path {:d "M28.188 1H3.812A2.821 2.821 0 0 0 1 3.813v24.375A2.821 2.821 0 0 0 3.813 31h24.375A2.821 2.821 0 0 0 31 28.187V3.812A2.821 2.821 0 0 0 28.187 1zM9.175 25.352a2.541 2.541 0 0 1-2.549-2.537 2.553 2.553 0 0 1 2.549-2.543 2.55 2.55 0 0 1 2.549 2.543 2.541 2.541 0 0 1-2.549 2.537zm6.399.023a8.913 8.913 0 0 0-2.62-6.339 8.882 8.882 0 0 0-6.328-2.625v-3.668c6.961 0 12.633 5.666 12.633 12.633h-3.685v-.001zm6.51 0c0-8.526-6.932-15.469-15.451-15.469V6.239c10.546 0 19.13 8.589 19.13 19.137h-3.68v-.001z", :fill-rule "nonzero"}]] [:symbol {:viewbox "0 0 32 32", :fill-rule "evenodd", :clip-rule "evenodd", :stroke-linejoin "round", :stroke-miterlimit "1.414", :id "twitter", :xmlns "http://www.w3.org/2000/svg"} [:path {:d "M31.003 6.695c-1.102.492-2.291.82-3.533.966a6.185 6.185 0 0 0 2.706-3.404 12.404 12.404 0 0 1-3.908 1.495 6.154 6.154 0 0 0-4.495-1.94 6.153 6.153 0 0 0-5.994 7.553A17.468 17.468 0 0 1 3.093 4.932a6.15 6.15 0 0 0-.831 3.094 6.147 6.147 0 0 0 2.736 5.122 6.16 6.16 0 0 1-2.789-.768v.076a6.154 6.154 0 0 0 4.94 6.034 6.149 6.149 0 0 1-2.783.106 6.177 6.177 0 0 0 5.748 4.277 12.347 12.347 0 0 1-9.117 2.549 17.4 17.4 0 0 0 9.44 2.766c11.32 0 17.513-9.381 17.513-17.514 0-.269-.005-.533-.017-.796a12.405 12.405 0 0 0 3.07-3.182v-.001z", :fill-rule "nonzero"}]]]]
+      [:nav.navbar {:role "navigation" :aria-label "main navigation"}
+       [:div.navbar-brand
+        [:a.navbar-item {:href "https://sill.etalab.gouv.fr"}
+         [:img {:src    "/images/logo-marianne.svg"
+                :alt    "Logo Marianne"
+                :width  "120"
+                :height "100"}
+          "sill.etalab.gouv.fr (alpha)"]]]
+       [:div.navbar-end
+        [:div.navbar-menu
+         [:a.navbar-item
+          {:href (str "/" lang "/contributors")
+           :title (i/i lang [:contributors-baseline])} (i/i lang [:contributors])]
+         [:a.navbar-item
+          {:href (str "/" lang "/contact")
+           :title (i/i lang [:contact-baseline])} (i/i lang [:contact])]
+         [:a.navbar-item
+          {:href (str "/" lang "/about")
+           :title (i/i lang [:why-this-website?])} (i/i lang [:about])]
+         [:a.navbar-item
+          {:href  "https://www.etalab.gouv.fr"
+           :title (i/i lang [:main-etalab-website])} "Etalab"]
+         [:a {:href  "/latest.xml" :target "new"
+              :title (i/i lang [:subscribe-rss-flux])
+              :class "navbar-item button"} [:span {:class "icon"}
+                                            [:i {:class "fas fa-rss"}]]]]]]
+      [:section.hero
+       [:div.hero-body
+        [:div.container
+         [:h1.title.has-text-centered title]
+         [:h2.subtitle.column.is-8.is-offset-2.has-text-centered subtitle]]]]
+      [:section.section content0]
+      (when-not content
+        [:div [:script {:src "/js/sillweb.js"}]
+         [:script "sillweb.core.init();"]])
+      [:footer.footer
+       [:div.content
+        [:div.columns
+         [:div.column.is-offset-2.is-4
+          [:img {:src "/images/etalab.svg" :width "240px"}]
+          [:ul {:class "footer__social"}
+           [:li [:a {:href "https://twitter.com/etalab", :title "Twitter"}
+                 [:svg {:class "icon icon-twitter"} [:use {:xlink:href "#twitter"}]]]]
+           [:li [:a {:href "https://github.com/etalab", :title "Github"}
+                 [:svg {:class "icon icon-github"} [:use {:xlink:href "#github"}]]]]
+           [:li [:a {:href "https://www.facebook.com/etalab", :title "Facebook"}
+                 [:svg {:class "icon icon-fb"} [:use {:xlink:href "#facebook"}]]]]
+           [:li [:a {:href "mailto:info@data.gouv.fr", :title "To send us an email"} ;; FIXME
+                 [:svg {:class "icon icon-mail"} [:use {:xlink:href "#envelope"}]]]]]]
+         [:div.column.is-offset-1.is-4
+          [:h1 "sill.etalab.gouv.fr"]
+          [:p (i/i lang [:website-developed-by])
+           [:a {:href "https://www.etalab.gouv.fr"} "Etalab"]
+           [:a {:href "https://github.com/etalab/sillweb"}
+            (i/i lang [:source-code-available]) (i/i lang [:here]) "."]]]]]]])))
 
 (defn contact [lang]
-  (template
+  (default
    lang
    (i/i lang [:contact-form])
    (i/i lang [:contact-baseline])
@@ -151,8 +187,8 @@
         :value (i/i lang [:submit])}]]]
     [:br]]))
 
-(defn ok [lang] ;; FIXME: unused
-  (template
+(defn ok [lang]
+  (default
    lang
    (i/i lang [:message-received])
    (i/i lang [:message-received-ok])
@@ -171,27 +207,27 @@
 
 (defn contributors [lang contributors]
   (condp = lang
-    "fr" (template
+    "fr" (default
           lang
           "Les organismes publics qui contribuent au SILL"
           [:span [:a {:href (str "/" lang "/contact")} "Contactez-nous"] " pour participer."]
           (show-contributors contributors))
-    "en" (template
+    "en" (default
           lang
           "Public sector agencies contributing to sill.etalab.gouv.fr" ""
           (show-contributors contributors))
-    "de" (template
+    "de" (default
           lang
           "Beitragszahler des öffentlichen Sektors für sill.etalab.gouv.fr" ""
           (show-contributors contributors))
-    "es" (template
+    "es" (default
           lang
           "Agencias del sector público contribuyendo a sill.etalab.gouv.fr" ""
           (show-contributors contributors))))
 
 (defn about [lang]
   (condp = lang
-    "fr" (template
+    "fr" (default
           lang
           "À propos de sill.etalab.gouv.fr" ""
           [:div.container
@@ -208,7 +244,7 @@
            (md-to-string "Vous trouverez des informations plus détaillées sur [cette page](https://disic.github.io/sill/index.html).")
            [:br]
            ])
-    "en" (template
+    "en" (default
           lang
           "About sill.etalab.gouv.fr" ""
           [:div.container
@@ -225,7 +261,7 @@
            (md-to-string "You can find more detailed information on [this page](https://disic.github.io/sill/index.html).")
            [:br]
            ])
-    "de" (template
+    "de" (default
           lang
           "Über sill.etalab.gouv.fr" ""
           [:div.container
@@ -241,7 +277,7 @@
            [:h2.subtitle "Wie kann ich mehr über den SILL erfahren ?"]
            (md-to-string "Weitere Informationen finden Sie [hier](https://disic.github.io/sill/index.html).")
            [:br]])
-    "es" (template
+    "es" (default
           lang
           "Sobre sill.etalab.gouv.fr" ""
           [:div.container
@@ -258,7 +294,7 @@
            (md-to-string "Puedes encontrar información más detallada [aquí](https://disic.github.io/sill/index.html).")
            [:br]
            ])
-     "it" (template
+    "it" (default
           lang
           "A proposito di sill.etalab.gouv.fr" ""
           [:div.container
